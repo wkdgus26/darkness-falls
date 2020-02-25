6442ea2d88d8e2f2fa26c2318b0c92c2567ceaf9
GpsWalika <44572131+GpsWalika@users.noreply.github.com>
2020-02-25 11:55:40 +0900

    kimmojji
    모래, 물, 조약돌 심어놨음

---
+++ Assets/Script/Light/MouseEvent(구현중).cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvent : MonoBehaviour {
+    private Vector3 mousePosition;
+    private Vector3 objPosition;
+    public float dis = 10;
+	void OnMouseDrag()
+    {
+        mousePosition = new Vector3(Input.mousePosition.x,
+            Input.mousePosition.y, dis);
+        objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+}
