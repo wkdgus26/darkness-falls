10ac85d4124696dd9e391cc2c06a3305b2720f50
GpsWalika <44572131+GpsWalika@users.noreply.github.com>
2020-02-29 20:42:27 +0900

    Jinhyeok
    1

---
+++ Assets/Script/Player/Move/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private float cXPosition = 0f;
+    private GameObject Player;
+    private Camera camera;
+    
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        Player = gameObject;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+            StartCoroutine("playerMove");
+        
+
+    }
+    
+    IEnumerator playerMove()
+    {
+        Vector2 speed = Vector2.zero;
+
+        Vector2 msPos = camera.ScreenToWorldPoint(Input.mousePosition);
+        while (gameObject.transform.position.x <= msPos.x)
+        {
+
+            if (gameObject.transform.position.x == msPos.x)
+                Debug.Log("OH");
+            gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position
+                    , msPos, Time.smoothDeltaTime * 3f);
+            msPos.x += 0.1f;
+            yield return null;
+        }
+        
+    }
+}
+
