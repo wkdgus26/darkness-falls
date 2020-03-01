41fc7ab82855a4dbbcd62c9ea02bb6b71cec00d8
GpsWalika <44572131+GpsWalika@users.noreply.github.com>
2020-03-01 13:00:13 +0900

    Test
    12

---
+++ Assets/Script/Minimap/ch1Mp/FadeIn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour {
+
+    float time = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < 3f)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, time / 3);
+        }
+        time += Time.deltaTime;
+
+    }
+}
---
+++ Assets/Script/Player/Move/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private float cXPosition = 0f;
+    private GameObject Player;
+    private Camera camera;
+    
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        Player = gameObject;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        StartCoroutine("playerMove");
+    }
+    
+    IEnumerator playerMove()
+    {
+        Vector2 speed = Vector2.zero;
+
+        Vector2 msPos = camera.ScreenToWorldPoint(Input.mousePosition);
+        
+        while (gameObject.transform.position.x <= msPos.x)
+        {
+            if (gameObject.transform.position.x == msPos.x)
+                Debug.Log("OH");
+             Player.transform.position = Vector2.MoveTowards(gameObject.transform.position, new Vector2 (msPos.x, gameObject.transform.position.y), Time.smoothDeltaTime * 3f);
+            //Player.transform.position = Vector2.Lerp(gameObject.transform.position, new Vector2 (msPos.x, gameObject.transform.position.y), Time.smoothDeltaTime * 3f);
+            //msPos.x += 0.1f;
+            yield return null;
+        }
+        
+       // Debug.Log(msPos);
+       // yield return null;
+    }
+}
+
