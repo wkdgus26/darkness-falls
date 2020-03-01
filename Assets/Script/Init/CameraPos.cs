55fb9c43aa71913f86db80121e1dbcae6cee362a
GpsWalika <44572131+GpsWalika@users.noreply.github.com>
2020-03-01 22:42:04 +0900

    1
    1

--- Assets/Script/Init/CameraPos.cs
+++ Assets/Script/Init/CameraPos.cs
@@ -7,12 +7,12 @@ public class CameraPos : MonoBehaviour {
     private GameObject Target;
     public float CameraZ = -10;
 
-    void FixedUpdate() //실행순서 업데이트 다음에 실행함
+    void FixedUpdate()
     {
         Vector3 TargetPos = new Vector3(
-            Mathf.Clamp(player.transform.position.x, -5.8f, 5.8f), //지금 x의 값이 
+            Mathf.Clamp(player.transform.position.x, -5.8f, 5.8f), 
             Mathf.Clamp(player.transform.position.y, -1.1f, 1.05f), 
             CameraZ);
-        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * 1f); //움직일때 쓰는거 보간
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * 1f);
     }
 }
