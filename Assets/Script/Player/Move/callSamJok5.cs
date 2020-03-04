b0af6d8fe3285753c6971517b1b92093a18bb775
GpsWalika <44572131+GpsWalika@users.noreply.github.com>
2020-03-04 20:30:04 +0900

    123
    123

---
+++ Assets/Script/Player/Move/callSamJok5.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class callSamJok5 : MonoBehaviour {
+    public GameObject samjok5;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("EventTrig1"))
+            samjok5.SetActive(true);
+    }
+}
