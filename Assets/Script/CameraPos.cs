b1c2e99e3666e3816e83036319c97989d72c7a16
wkdgus26 <51107877+wkdgus26@users.noreply.github.com>
2020-03-12 22:01:19 +0900

    123
    123

---
+++ Assets/Script/CameraPos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPos : MonoBehaviour {
+    public GameObject player;
+    private GameObject Target;
+    public float CameraZ = -10;
+    
+    void FixedUpdate()
+    {
+        Vector3 TargetPos = new Vector3(
+            Mathf.Clamp(player.transform.position.x, -5.88f, 5.88f), 
+            Mathf.Clamp(player.transform.position.y, -1.27f, 1.27f), 
+            CameraZ);
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * 1f);
+    }
+}
---
+++ Assets/Script/Parallax scroll/parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour {
+    private float startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+    
+	// Use this for initialization
+	void Start () {
+        startpos = transform.position.x;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+	}
+}
