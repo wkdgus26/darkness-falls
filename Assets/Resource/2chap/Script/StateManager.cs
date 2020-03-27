e028e7cd8e8fe2ffe14362acfaf9ef0f0b2406e0
GpsWalika <44572131+GpsWalika@users.noreply.github.com>
2020-03-27 09:58:11 +0900

    123
    123

--- Assets/Resource/2chap/Script/StateManager.cs
+++ Assets/Resource/2chap/Script/StateManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateManager{
+public class StateManager : MonoBehaviour
+{
 	public bool isMove = true;
 	public bool nextMap = false;
 }
