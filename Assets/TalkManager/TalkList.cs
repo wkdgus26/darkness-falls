f676a544c707d6827a3c27034a01d3a90a7c329e
GpsWalika <44572131+GpsWalika@users.noreply.github.com>
2020-04-02 13:23:56 +0900

    123
    123

---
+++ Assets/TalkManager/TalkList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkList : MonoBehaviour {
+	private Queue<string> textList;
+
+	// Use this for initialization
+	void Start () {
+		textList = new Queue<string>();
+	}
+}
