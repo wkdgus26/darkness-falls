5d592043d293f56a199346c1936932978c03566c
wkdgus26 <51107877+wkdgus26@users.noreply.github.com>
2020-04-16 14:48:02 +0900

    13
    13

---
+++ Assets/bookduController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bookduController : MonoBehaviour {
+
+	public GameObject[] bookdu;
+	int index = 0;
+	float time = 0;
+	bool isFade = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (index < bookdu.Length)
+		{
+			time += Time.deltaTime;
+			if (time > 0.1f)
+			{
+				bookdu[index].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, time / 0.1f);
+				//bookdu[index].SetActive(true);
+				if (time > 0.2f)
+				{
+					if (!isFade)
+					{
+						StartCoroutine(fadeOutCoroutine());
+						index++;
+						time = 0;
+					}
+				}
+			}
+		}
+	}
+
+	IEnumerator fadeOutCoroutine()
+	{
+		isFade = true;
+		float atime = 0.2f;
+		int a = index;
+
+		while (atime > 0)
+		{
+			atime -= Time.deltaTime;
+			bookdu[a].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, atime / 0.6f);
+			yield return null;
+		}
+		bookdu[a].SetActive(false);
+		isFade = false;
+	}
+}
+
+
